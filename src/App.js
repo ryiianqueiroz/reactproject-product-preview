50524a3e6ee9d02b8b4e6353943056c811760e4d
ryan <ryanqueiroz2012@gmail.com>
2023-06-30 20:05:42 -0300

    Subindo projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Photo from './Model/images/image-product-desktop.jpg'
+import Cart from './Model/images/icon-cart.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="photo">
+          <img src={Photo} alt="#" id="photoPrincipal" />
+        </div>
+        <div className="textContent">
+          <h5>PREVIEW</h5>
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+          <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, 
+          Perfumer-Creator for the House of CHANEL.</p>
+          <div className="prices">
+            <h2>$149.99</h2>
+            <s>$169.99</s>
+          </div>
+          <button><img src={Cart} alt="#" id="cart"/> Add To Cart</button>
+        </div>
+      </div>
     </div>
   );
 }
